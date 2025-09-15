af8379c5c2e9ced1c1723a15de56be1d50ce5f7b
ANSHAD  P P <71533560+anshadpp@users.noreply.github.com>
2025-09-15 18:47:14 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [react(), runtimeErrorOverlay()],
+  base: "/portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
